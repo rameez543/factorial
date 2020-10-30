13e4f006ddb817ea45b1e6e2a50b106218786b60
Harpreetkaurpanesar25 <52062960+Harpreetkaurpanesar25@users.noreply.github.com>
2020-10-30 22:16:50 +0530

    using while loop

--- factorial.py
+++ factorial.py
@@ -3,3 +3,15 @@ fact=1
 for i in range(1,n+1):
       fact=fact*i
 print(fact)
+
+
+#using while loop
+n=int(input("enter the number : "))
+f=1
+if (n==0):
+    print("1")
+else:
+    for i in range(1,n+1):
+        f=f*i
+        print("The factorial of ",i,"is",f)
+
